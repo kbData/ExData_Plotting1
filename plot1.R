4c914efcccf88481038435e65c3ac2fa612a885c
KirylBatsiukov <kiryl.batsiukov@gmail.com>
2014-07-09 13:52:33 +0200

    added plot1

---
+++ plot1.R
@@ -0,0 +1,5 @@
+source("./readData.R")
+data<-readData()
+png(file="plot1.png")
+hist(data$Global_active_power,col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
