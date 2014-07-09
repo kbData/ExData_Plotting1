b5592b3bfc766dbd13ef35167df20a1de82c806f
KirylBatsiukov <kiryl.batsiukov@gmail.com>
2014-07-09 13:39:15 +0200

    added caching to readData

--- readData.R
+++ readData.R
@@ -1,16 +1,26 @@
-readData<- function(){
+readData<- function(){        
+        require(R.cache)
+        cacheKey = list("ELECTRICITY")
+        dataCsv<-loadCache(key=cacheKey,removeOldCache=true, sources="./readData.R")
+        if(!is.null(dataCsv))
+        {
+                message("loading data from cache")
+                return (dataCsv)
+        }
         require(sqldf)
-        fileName<-"consumption.zip"
-        if(!file.exists(fileName))
+        zipFileName<-"consumption.zip" 
+        if(!file.exists(zipFileName))
         {
                 download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
                               dest="consumption.zip", method="curl")
                 
-        }    
-        if(!file.exists("household_power_consumption.txt"))
-        {
-                unzip(fileName)
         }
+        fileName<-"household_power_consumption.txt"
+        if(!file.exists(fileName))
+        {
+                unzip(zipFileName)
+        }
+        
         dataCsv<-read.csv.sql("household_power_consumption.txt", sep=";", sql="select * from file where Date in (\"1/2/2007\",\"2/2/2007\")")
         library(lubridate)
         
@@ -18,5 +28,7 @@ readData<- function(){
         dataCsv[,1]<-dmy(dataCsv[,1])
         dataCsv[,2]<-hms(dataCsv[,2])
         
+        saveCache(dataCsv, key=cacheKey)
+        message("saved data to cache")
         dataCsv
 }
