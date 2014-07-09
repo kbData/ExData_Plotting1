8c583d61b990027f007ccbae50d62345e3aef4e3
KirylBatsiukov <kiryl.batsiukov@gmail.com>
2014-07-09 14:56:37 +0200

    added plot 3

---
+++ plot3.R
@@ -0,0 +1,11 @@
+source("./readData.R")
+data<-readData()
+png(file="plot3.png")
+with(data,
+        {
+        plot(datetime, Sub_metering_1, type="l", xlab="",ylab="Energy sub metering")
+        lines(datetime, Sub_metering_2, col="red")
+        lines(datetime, Sub_metering_3, col="blue")
+})
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red", "blue"),lty=1)
+dev.off()
