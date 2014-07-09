2682ae029d492b8dd1eaa527c76af6b240252e9b
KirylBatsiukov <kiryl.batsiukov@gmail.com>
2014-07-09 14:12:24 +0200

    added plot2

---
+++ plot2.R
@@ -0,0 +1,5 @@
+source("./readData.R")
+data<-readData()
+png(file="plot2.png")
+with(data, plot(data$datetime, data$Global_active_power, type="l", xlab="",ylab="Global Active Power (kilowatts)", main="Global Active Power"))
+dev.off()
